bb79601d3c965979839c4658a4e9410ad21842c2
ched <ched5@orange.fr>
2022-04-09 09:40:28 +0200

    create project react

---
+++ checkp2/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1 className='header-title'>WILD GAGAMES</h1>
+            <p className='header-text'>Venez découvrir notre l'univers et nos jeux... Pret pour l'aventure !!! </p>
+        </div>
+    );
+};
+
+export default Header;
